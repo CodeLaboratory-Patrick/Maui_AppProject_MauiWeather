8644b28a102d035c2e56169e95301ebe4b8a97b1
Donghwi_Park <gptnc5100@gmail.com>
2024-10-24 19:52:36 +0200

    Obtaining the weather of a place.

--- MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
+++ MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MauiWeather.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -9,11 +11,40 @@ namespace MauiWeather.MVVM.ViewModels
 {
     public class WeatherViewModel
     {
+        public WeatherData WeatherData { get; set; }
+        private HttpClient client;
+
+        public WeatherViewModel()
+        {
+            client = new HttpClient();
+        }
+
         public ICommand SearchCommand =>
-            new Command(async (searchText) =>
+               new Command(async (searchText) =>
+               {
+                   var location =
+                        await GetCoordinatesAsync(searchText.ToString());
+                   await GetWeather(location);
+               });
+
+        private async Task GetWeather(Location location)
+        {
+            var url = 
+                $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&current=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin";
+
+            var response =
+                 await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
             {
-                var location = await GetCoordinatesAsync(searchText.ToString());
-            });
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer
+                         .DeserializeAsync<WeatherData>(responseStream);
+                    WeatherData = data;
+                }
+            }
+        }
 
         private async Task<Location> GetCoordinatesAsync(string address)
         {
