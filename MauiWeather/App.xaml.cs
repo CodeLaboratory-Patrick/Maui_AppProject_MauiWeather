78f53f16ff98d1bf64bc72aef7b6c4618518a464
Donghwi_Park <gptnc5100@gmail.com>
2024-10-24 18:38:41 +0200

    Add project files.

---
+++ MauiWeather/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWeather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
